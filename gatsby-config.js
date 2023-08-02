5b47a45ff219dddc147109c4b1a15cec768732b1
Shoaib <shoaibzkhalid@gmail.com>
2023-08-02 06:22:50 +0500

    - improved the site

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,61 @@ module.exports = {
     {
       resolve: '@wkocjan/gatsby-theme-intro',
       options: {
+        contentPath: 'content/',
         theme: 'classic',
         showThemeLogo: false,
-        icon: `content/images/favicon.png`, // Replace with the path to your icon fil
-        // icon: `content/images/favicon.ico`, // Replace with the path to your icon fil
+      },
+    },
+    // {
+    //   resolve: 'gatsby-plugin-manifest',
+    //   options: {
+    //     icon: `/static/favicon.ico`,
+
+    //     icons: [
+    //       {
+    //         src: `/static/images/favicon.png`,
+    //         sizes: `32x32`,
+    //         type: `image/png`,
+    //       },
+    //     ],
+    //   },
+    // },
+
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
+        web: [
+          {
+            name: `Roboto`,
+            file: `/static/fonts/roboto/500.css`,
+          },
+          {
+            name: `Roboto`,
+            file: `/static/fonts/roboto/400.css`,
+          },
+          {
+            name: `Open Sans`,
+            file: `/static/fonts/open-sans/300.css`,
+          },
+          {
+            name: `Open Sans`,
+            file: `/static/fonts/open-sans/400.css`,
+          },
+          {
+            name: `Open Sans`,
+            file: `/static/fonts/open-sans/600.css`,
+          },
+          {
+            name: `Open Sans`,
+            file: `/static/fonts/open-sans/700.css`,
+          },
+          {
+            name: `Open Sans`,
+            file: `/static/fonts/open-sans/300.css`,
+          },
+        ],
       },
     },
   ],
